f7723eadd336792425de6387d88695a51a2d77d4
drspacemanphd <markabrown87@gmail.com>
2020-12-16 05:47:15 -0500

    Adding int tests

---
+++ packages/podcast-radio-podcast-dao/tests/integration/config/dynamo-db.test.ts
@@ -0,0 +1,12 @@
+import { DynamoDB } from 'aws-sdk'
+import { configureDynamoDB } from '@drspacemanphd/podcast-radio-podcast-dao/src/config/dynamo-db';
+
+describe('DynamoDB Config', () => {
+  test('it should be setup properly', () => {
+    const env: Record<string, any> = process.env;
+    expect(env.DYNAMODB_ENDPOINT).toEqual('http://localstack:4566');
+    expect(env.DYNAMODB_REGION).toEqual('us-east-1');
+    const DB = configureDynamoDB(env)
+    expect(DB instanceof DynamoDB.DocumentClient).toBeTruthy();
+  });
+});
---
+++ packages/podcast-radio-podcast-dao/tests/integration/setup.ts
@@ -0,0 +1,1 @@
+require('custom-env').env('local');
