4b61f9f225cadc2bed8d840cf3677f4e91646156
drspacemanphd <markabrown87@gmail.com>
2021-02-13 13:37:00 -0500

    script

---
+++ scripts/release.sh
@@ -0,0 +1,7 @@
+if [ -z "$bump" ]; then
+  echo MUST PROVIDE VALID LERNA BUMP FOR VERSIONING
+  return 1
+else 
+  lerna version patch && lerna run build && lerna publish from-git --yes --contents dist
+  return 0
+fi
