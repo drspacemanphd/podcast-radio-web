e9326e2831371797f4b2a163eacfa5daf07dae51
drspacemanphd <markabrown87@gmail.com>
2021-02-13 20:19:58 -0500

    new scripts and packages

---
+++ scripts/clean.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -rf ./node_modules
+rm -rf ./packages/**/node_modules
+rm -rf ./packages/**/dist
+rm -rf ./services/**/node_modules
+rm -rf ./services/**/dist
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,11 @@
+#!/bin/sh
+
 if [ -z "$BUMP" ]; then
   echo MUST PROVIDE VALID LERNA BUMP FOR VERSIONING
   return 1
-else 
-  lerna version patch && lerna run build && lerna publish from-git --yes --contents dist
+else
+  npm run clean
+  npm run build
+  lerna version "$BUMP" && lerna run build && lerna publish from-git --yes --contents dist
   return 0
 fi
