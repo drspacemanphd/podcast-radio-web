145e2ed959243b0ae6a6aed4066fda850dd6830d
drspacemanphd <markabrown87@gmail.com>
2021-02-13 20:27:32 -0500

    scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+set -e
 
 if [ -z "$BUMP" ]; then
   echo MUST PROVIDE VALID LERNA BUMP FOR VERSIONING
   return 1
 else
   npm run clean
-  yarn --frozen-lockfile --production
-  lerna run build
-  lerna version "$BUMP" && lerna run build && lerna publish from-git --yes --contents dist
+  yarn --frozen-lockfile
+  lerna version "$BUMP"
+  lerna publish from-git --yes --contents dist
   return 0
 fi
