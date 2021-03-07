4564df1db1ab59e96c5ee1c408eaee6057776502
drspacemanphd <markabrown87@gmail.com>
2021-03-07 10:12:58 -0500

    Simplify release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 set -e
 
-if [ -z "$BUMP" ]; then
-  echo MUST PROVIDE VALID LERNA BUMP FOR VERSIONING
-  return 1
-else
-  npm run clean
-  yarn --frozen-lockfile
-  lerna version --conventional-commits --yes
-  lerna run build
-  lerna publish from-git --contents ./dist --yes
-  return 0
-fi
+npm run clean
+yarn --frozen-lockfile
+lerna version --conventional-commits --yes
+lerna run build
+lerna publish from-git --contents ./dist --yes
+return 0
