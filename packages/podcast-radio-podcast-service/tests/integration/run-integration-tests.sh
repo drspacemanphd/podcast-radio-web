a5324faf7d88bcd22622322e28b0c1213c3534a9
drspacemanphd <markabrown87@gmail.com>
2021-03-11 22:37:30 -0500

    trying v14

--- packages/podcast-radio-podcast-service/tests/integration/run-integration-tests.sh
+++ packages/podcast-radio-podcast-service/tests/integration/run-integration-tests.sh
@@ -9,6 +9,6 @@ npm run build
 (cd ./infrastructure/integration-tests/podcast-radio-podcast-service && rm -rf terraform*)
 (cd ./infrastructure/integration-tests/podcast-radio-podcast-service && terraform init)
 (cd ./infrastructure/integration-tests/podcast-radio-podcast-service && terraform apply -auto-approve)
-unzip lambda.zip -d ./lambda
+unzip lambda.zip -d ./lambda -q
 (cd ./lambda && ls -al)
 jest --testMatch=**/integration/**/*.test.ts --runInBand --detectOpenHandles --verbose --testTimeout=60000
