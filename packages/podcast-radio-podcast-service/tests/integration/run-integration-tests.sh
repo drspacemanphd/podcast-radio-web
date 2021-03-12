f02249fafc9c7b1f3afe08e57d04a4cf4a785e52
drspacemanphd <markabrown87@gmail.com>
2021-03-11 22:31:40 -0500

    trying v14

--- packages/podcast-radio-podcast-service/tests/integration/run-integration-tests.sh
+++ packages/podcast-radio-podcast-service/tests/integration/run-integration-tests.sh
@@ -2,10 +2,13 @@
 set -e
 
 apk add zip
+apk add unzip
 npm run build
 (cd ./dist && zip -r9q ../lambda.zip  ./* ../node_modules ../.env.integration)
 (cd ./infrastructure/integration-tests/podcast-radio-podcast-service && rm -rf .terraform)
 (cd ./infrastructure/integration-tests/podcast-radio-podcast-service && rm -rf terraform*)
 (cd ./infrastructure/integration-tests/podcast-radio-podcast-service && terraform init)
 (cd ./infrastructure/integration-tests/podcast-radio-podcast-service && terraform apply -auto-approve)
+unzip lambda.zip -d ./lambda
+(cd ./lambda && ls -al)
 jest --testMatch=**/integration/**/*.test.ts --runInBand --detectOpenHandles --verbose --testTimeout=60000
