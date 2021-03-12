0731af6ae4e977c93c3f216f4558871f6ba6dc40
drspacemanphd <markabrown87@gmail.com>
2021-03-12 07:47:53 -0500

    trying local mount

--- infrastructure/integration-tests/podcast-radio-podcast-service/main.tf
+++ infrastructure/integration-tests/podcast-radio-podcast-service/main.tf
@@ -9,27 +9,27 @@ module "podcast_update_queue" {
   account_id                  = "000000000000"
 }
 
-# module "lambda_function" {
-#   source            = "../../common/lambda_from_local"
-#   service_name      = "podcast-radio-podcast-service"
-#   environment       = "integration"
-#   lambda_code_path  = "../../../lambda.zip"
-#   description       = "microservice that updates podcast entries and assets"
-#   lambda_variables  = {
-#     NODE_ENV = "integration"
-#   }
-# }
-
 module "lambda_function" {
-  source            = "../../common/lambda_from_s3"
+  source            = "../../common/lambda_from_local"
   service_name      = "podcast-radio-podcast-service"
   environment       = "integration"
-  description       = "poller for rss feeds"
-  filename          = "../../../lambda.zip"
+  lambda_code_path  = "/tmp/lambda.zip"
+  description       = "microservice that updates podcast entries and assets"
   lambda_variables  = {
     NODE_ENV = "integration"
   }
 }
+
+# module "lambda_function" {
+#   source            = "../../common/lambda_from_s3"
+#   service_name      = "podcast-radio-podcast-service"
+#   environment       = "integration"
+#   description       = "poller for rss feeds"
+#   filename          = "../../../tmp/lambda.zip"
+#   lambda_variables  = {
+#     NODE_ENV = "integration"
+#   }
+# }
 
 
 resource "aws_lambda_event_source_mapping" "podcast_update_queue_mapping" {
--- packages/podcast-radio-podcast-service/tests/integration/run-integration-tests.sh
+++ packages/podcast-radio-podcast-service/tests/integration/run-integration-tests.sh
@@ -2,13 +2,11 @@
 set -e
 
 apk add zip
-apk add unzip
 npm run build
 (cd ./dist && zip -r9q ../lambda.zip  ./* ../node_modules ../.env.integration)
+cp ./lambda.zip /tmp
 (cd ./infrastructure/integration-tests/podcast-radio-podcast-service && rm -rf .terraform)
 (cd ./infrastructure/integration-tests/podcast-radio-podcast-service && rm -rf terraform*)
 (cd ./infrastructure/integration-tests/podcast-radio-podcast-service && terraform init)
 (cd ./infrastructure/integration-tests/podcast-radio-podcast-service && terraform apply -auto-approve)
-unzip -q lambda.zip -d ./lambda
-(cd ./lambda && ls -al)
 jest --testMatch=**/integration/**/*.test.ts --runInBand --detectOpenHandles --verbose --testTimeout=60000
