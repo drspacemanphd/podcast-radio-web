66a954539e11772713f40bb04ad3380a6b262613
drspacemanphd <markabrown87@gmail.com>
2021-03-13 22:00:57 -0500

    ignore

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -8,8 +8,8 @@ IFS=$'\n'
 readarray -t packages <<<"$changed" #reading str as an array as tokens separated by IFS  
 echo ''
 echo $packages
-for i in "${packages[@]}"; do   # access each element of array
-  package=${i:15}
-  echo $package
-  # (cd ./packages/$package && npm run test:integration)
-done
+# for i in "${packages[@]}"; do   # access each element of array
+#   package=${i:15}
+#   echo $package
+#   (cd ./packages/$package && npm run test:integration)
+# done
