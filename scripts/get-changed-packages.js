266b6bcfbdc92e5f5aca390f61fc33f76e00d8b7
drspacemanphd <markabrown87@gmail.com>
2021-03-13 21:40:50 -0500

    add script to dynamically set int tests to run

---
+++ scripts/get-changed-packages.js
@@ -0,0 +1,33 @@
+const { spawnSync, execSync } = require('child_process');
+
+const allPackages = spawnSync('lerna', ['ls']).stdout.toString('utf-8').split(/\n/);
+
+const diffedFiles = spawnSync('git', ['diff', 'master', '--name-only']);
+if (diffedFiles.error) {
+  throw new Error(diffedFiles.error.stack);
+}
+
+const diffedPackages = diffedFiles.stdout.toString('utf-8')
+  .split(/\n/)
+  .map(file => {
+    if (file.startsWith('packages')) {
+      const match = file.match(/\/([a-zA-Z-]+)\//);
+      return `@drspacemanphd/${match[1]}`;
+    } else if (file.includes('/')) {
+      const match = file.match(/[a-zA-Z-]+/);
+      return match.toString();
+    } else {
+      return file;
+    }
+  })
+  .filter(file => {
+    return file.startsWith('infrastructure') || file.startsWith('@drspacemanphd');
+  });
+
+const packageSet = Array.from(new Set(diffedPackages));
+
+if (packageSet.includes('infrastructure')) {
+  process.stdout.write(allPackages.join('\n'));
+} else {
+  process.stdout.write(packageSet.join('\n'));
+}
--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-changed=$(set CI=false && lerna changed)
-echo $changed
+changed=$(node ./scripts/get-changed-packages.js)
 
 IFS=$'\n'
 readarray -t packages <<<"$changed" #reading str as an array as tokens separated by IFS  
-  
+echo ''
+echo $packages
 for i in "${packages[@]}"; do   # access each element of array
   package=${i:15}
+  echo $package
   (cd ./packages/$package && npm run test:integration)
 done
