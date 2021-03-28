c98b802622c607918b98db2ba225b2d6e078a4f5
drspacemanphd <markabrown87@gmail.com>
2021-03-28 10:59:39 -0400

    local

--- infrastructure/local/podcast-radio-rss-poller/outputs.tf
+++ infrastructure/local/podcast-radio-rss-poller/outputs.tf
@@ -1,15 +1,15 @@
 output dynamodb_podcast_table_name {
-  value       = module.dynamodb_table.dynamodb_podcast_table_name
+  value       = module.podcast_table.dynamodb_podcast_table_name
 }
 
 output dynamodb_episode_table_name {
-  value       = module.dynamodb_table.dynamodb_episode_table_name
+  value       = module.episode_table.dynamodb_episode_table_name
 }
 
 output dynamodb_rss_schedule_table_name {
-  value       = module.dynamodb_table.dynamodb_rss_schedule_table_name
+  value       = module.rss_schedule_table.dynamodb_rss_schedule_table_name
 }
 
 output dynamodb_rss_schedule_table_stream_arn {
-  value       = module.dynamodb_table.dynamodb_rss_schedule_table_stream_arn
+  value       = module.rss_schedule_table.dynamodb_rss_schedule_table_stream_arn
 }
